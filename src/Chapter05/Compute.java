8c53daabf23487994f2fa4b38d406fdbbd773cc3
Unknown <tmd9107@naver.com>
2018-08-10 21:21:27 +0900

    Chapter05 : 커맨드라인 실행
    실행시 args배열에 아규먼트를 직접 입력받아 실행하기

---
+++ src/Chapter05/Compute.java
@@ -0,0 +1,38 @@
+package Chapter05;
+
+public class Compute {
+
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]), 
+			b = Integer.parseInt(args[2]),
+			rs = 0;
+		String operator = args[1];
+		switch(operator){
+		case "+":
+			rs = a+b;
+			break;
+		case "-":
+			rs = a-b;
+			break;
+		case "@":
+			rs = a*b;
+			break;
+		case "/":
+			rs = a/b;
+			break;
+		default:
+			break;
+		}		
+		
+//		if(operator == "+"){
+//			rs = a+b;
+//		}else if(operator == "-"){
+//			rs = a-b;
+//		}else if(operator == "@"){
+//			rs = a*b;
+//		}else if(operator == "/"){
+//			rs = a/b;
+//		}
+		System.out.println(a+operator+b+"="+rs);
+	}
+}
